493d71057613248b859ca2341717fbba9c9a1546
Yaroslav Hromovyi <gromovoy88@gmail.com>
2018-09-02 11:07:53 +0200

    Adding support of Images

---
+++ README.md
@@ -0,0 +1,4 @@
+# Webpack-4-Boilerplate
+Webpack 4 Boilerplate for HTML development.
+
+Included: HTML + JS (Babel transpile) + SCSS modules. 
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./scss/main.scss";
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "./assets/img/img.jpg";
+
+console.log("hello, world");
